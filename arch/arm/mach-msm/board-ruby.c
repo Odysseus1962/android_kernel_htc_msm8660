0bf50509b755ae4c9c081c5c107e2e8016a87919
sportsstar89 <sjpirri@gmail.com>
2013-05-18 01:00:51 -0400

    board-ruby: fix typo
    add missing comma

--- arch/arm/mach-msm/board-ruby.c
+++ arch/arm/mach-msm/board-ruby.c
@@ -542,7 +542,7 @@ static struct regulator_init_data saw_s0_init_data = {
 		.constraints = {
 			.name = "8901_s0",
 			.valid_ops_mask = REGULATOR_CHANGE_VOLTAGE,
-			.min_uV = 700000
+			.min_uV = 700000,
 			.max_uV = 1450000,
 		},
 		.consumer_supplies = vreg_consumers_8901_S0,
