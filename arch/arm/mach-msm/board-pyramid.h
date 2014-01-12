e8a3fd160dc47de98b3d929abc21aee7bd9e020f
Matt Filetto <matt.filetto@gmail.com>
2014-01-12 00:03:25 -0800

    pyramid: increase PMEM SMI size
    Change-Id: Ie073c670d248a287f1cba245d52973770704a5b6
    
    Conflicts:
    
    	arch/arm/mach-msm/board-pyramid.h

--- arch/arm/mach-msm/board-pyramid.h
+++ arch/arm/mach-msm/board-pyramid.h
@@ -43,11 +43,21 @@
 #define MSM_ION_HEAP_NUM      5
 >>>>>>> 20e2e47... pyramid: ION: rebase off of sultan's updated heaps
 
+<<<<<<< HEAD
 #ifdef CONFIG_FB_MSM_HDMI_MSM_PANEL
 #define MSM_FB_EXT_BUF_SIZE  (1920 * 1080 * 2 * 1) /* 2 bpp x 1 page */
 #else
 #define MSM_FB_EXT_BUFT_SIZE	0
 #endif
+=======
+#define MSM_FB_SIZE roundup((960 * ALIGN(540, 32) * 4 * 3) + 0x3F4800, 4096)
+
+// PMEM SMI
+#define MSM_SMI_SIZE          0x4000000
+#define KERNEL_SMI_SIZE       0xE00000
+#define USER_SMI_SIZE         (MSM_SMI_SIZE - KERNEL_SMI_SIZE)
+#define MSM_PMEM_SMIPOOL_SIZE USER_SMI_SIZE
+>>>>>>> 067f6c1... pyramid: increase PMEM SMI size
 
 #ifdef CONFIG_FB_MSM_OVERLAY_WRITEBACK
 /* width x height x 3 bpp x 2 frame buffer */
@@ -116,7 +126,7 @@
 
 // Userspace allocation
 #define PHY_BASE_ADDR1  0x48000000
-#define SIZE_ADDR1      0x22900000
+#define SIZE_ADDR1      0x22100000
 /*** END Memory map ***/
 >>>>>>> 20e2e47... pyramid: ION: rebase off of sultan's updated heaps
 
